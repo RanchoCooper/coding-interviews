5f1aba44589fcd2d8c2b81a20e9e13e4fdab6bb0
Rancho <ranchocooper@gmail.com>
2019-09-09 19:13:10 +0800

    implement queue by double stack

---
+++ src/main/java/No07/DoubleStackQueue.java
@@ -0,0 +1,30 @@
+package No06.ConstructBinaryTree.No07;
+
+import java.util.Stack;
+
+/**
+ * @author rancho
+ * @date 2019/9/9
+ */
+public class DoubleStackQueue {
+
+    private Stack<Integer> stack1 = new Stack<>();
+    private Stack<Integer> stack2 = new Stack<>();
+
+    public void push(int node) {
+        stack1.add(node);
+    }
+
+    public int pop() {
+        if (stack1.isEmpty() && stack2.isEmpty()) {
+            return 0;
+        }
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+}
