7d9a452a357414201d49925d0a4063de688794c9
Rancho <ranchocooper@gmail.com>
2019-09-09 18:54:15 +0800

    construct binary tree

---
+++ src/main/java/No06/ConstructBinaryTree/Solution.java
@@ -0,0 +1,47 @@
+package No06.ConstructBinaryTree;
+
+import java.util.Arrays;
+
+/**
+ * @author rancho
+ * @date 2019/9/9
+ *
+ * 递归解法
+ */
+public class Solution1 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+
+        }
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        if (pre == null || in == null || pre.length == 0 || in.length == 0) {
+            return null;
+        }
+        if (pre.length != in.length) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(pre[0]);
+
+        for (int i = 0; i < pre.length; i++) {
+            if (pre[0] == in[i]) {
+                root.left = reConstructBinaryTree(Arrays.copyOfRange(pre, 1, i + 1), Arrays.copyOfRange(in, 0, i));
+                root.right = reConstructBinaryTree(Arrays.copyOfRange(pre, i + 1, pre.length), Arrays.copyOfRange(in, i + 1, in.length));
+            }
+        }
+        return root;
+
+    }
+
+}
